5cf8702a445739cd30f5985adc2f7b1f3aec0fd5
Diego Martinez <diegomrz@outlook.com>
2017-12-19 11:35:29 -0200

    Commit day 3 VII

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -5,7 +5,6 @@ module.exports = {
       .waitForElementVisible('.btn', 60000, true,
       	function(){}, 'Waiting for Search Input to become visible')
       .click('.btn')
-      .waitForElementPresent('.quantumWizButtonPaperbuttonLabel', 60000)
       .pause(5000)
       .end();
   }
