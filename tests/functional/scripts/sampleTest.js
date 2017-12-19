4a8cd2eb06bd9e227d274f5c4d52414c1c25afdf
Diego Martinez <diegomrz@outlook.com>
2017-12-19 11:29:51 -0200

    Commit day 3

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -3,6 +3,7 @@ module.exports = {
     browser
       .url('http://projectfindertest.mybluemix.net')
       .waitForElementVisible('.btn', 60000, true,
+      	function(){}, 'Waiting for Search Input to become visible')
       .click('.btn')
       .waitForElementPresent('.quantumWizButtonPaperbuttonLabel', 60000)
       .pause(5000)
