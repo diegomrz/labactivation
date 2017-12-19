0575b04c5a52975534fd240c7e17cce8b37d4f36
Diego Martinez <diegomrz@outlook.com>
2017-12-19 11:40:07 -0200

    Commit - Button message change

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url('http://projectfindertest.mybluemix.net')
       .waitForElementVisible('.btn', 60000, true,
-      	function(){}, 'Waiting for Search Input to become visible')
+      	function(){}, 'Waiting for button to be visible')
       .click('.btn')
       .pause(5000)
       .end();
