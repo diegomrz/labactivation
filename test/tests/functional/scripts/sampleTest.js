f6c90f43f4b9cffad8797959eab22b99dee0366a
Diego Martinez <diegomrz@outlook.com>
2017-12-19 10:44:45 -0200

    Commit day 3

---
+++ test/tests/functional/scripts/sampleTest.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'Sample functional test' : function (browser) {
+    browser
+      .url('http://projectfinder.mybluemix.net')
+      .waitForElementVisible('.btn', 60000, true,
+        function(){}, 'Waiting for Search Input to become visible')
+      .click('.btn')
+      .waitForElementPresent('.quantumWizButtonPaperbuttonLabel', 60000)
+      .pause(5000)
+      .end();
+  }
+};
